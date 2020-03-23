32fc05a557943afe0651abecf5871cf10c14ab42
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-03-23 10:16:22 +0100

    create base models / controllers

---
+++ BriqueArcASP/BriqueArcASP/Models/Ranking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BriqueArcASP.Models
+{
+    public class Ranking
+    {
+        public long RankingId { get; set; }
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+
+        public long UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
