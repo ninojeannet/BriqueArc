befd1cd93a8868eee8e358eae34824a1e601eaca
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-03-30 09:18:12 +0200

    add seeder

---
+++ BriqueArcASP/BriqueArcASP/Models/SeedData.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BriqueArcASP.Models
+{
+    public class SeedData
+    {
+        private static HashSet<User> setUsers;
+        private static HashSet<Ranking> setRankings;
+
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BriqueArcContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<BriqueArcContext>>()))
+            {
+                // Look for any events.
+                if (context.Users.Any())
+                {
+                    setUsers = context.Users.ToHashSet<User>();
+                    context.Users.RemoveRange(setUsers);
+                    //Modifier ici
+                }
+                if (context.Rankings.Any())
+                {
+                    setRankings= context.Rankings.ToHashSet<Ranking>();
+                    context.Rankings.RemoveRange(setRankings);
+                    //Modifier ici
+                }
+
+                User u1 = new User
+                {
+                    Username = "nino",
+                    Password = "admin",
+                };
+
+                User u2 = new User
+                {
+                    Username = "yohann",
+                    Password = "admin",
+                };
+                User u3 = new User
+                {
+                    Username = "kevin",
+                    Password = "admin",
+                };
+
+                Ranking r1 = new Ranking
+                {
+                    Date = DateTime.Today,
+                    Score = 100,
+                    User = u1,
+                    UserId = u1.UserId
+                };
+
+                Ranking r2 = new Ranking
+                {
+                    Date = DateTime.Today,
+                    Score = 10,
+                    User = u2,
+                    UserId = u2.UserId
+                };
+                Ranking r3 = new Ranking
+                {
+                    Date = DateTime.Today,
+                    Score = 50,
+                    User = u3,
+                    UserId = u3.UserId
+                };
+               
+              
+
+                context.Users.AddRange(u1, u2, u3);
+                context.Rankings.AddRange(r1, r2, r3);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
