bceeca8c71b8c6066b94b9ed7f1309e1d5f3a85e
Yohann.Loison <Yohann.Loison@he-arc.ch>
2020-05-02 10:56:57 +0200

    Commentaires RankingsController

--- BriqueArcASP/BriqueArcASP/Controllers/RankingsController.cs
+++ BriqueArcASP/BriqueArcASP/Controllers/RankingsController.cs
@@ -9,17 +9,28 @@ using BriqueArcASP.Models;
 
 namespace BriqueArcASP.Controllers
 {
+    /// <summary>
+    /// Controleur de tout ce qui concerne les résultats
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class RankingsController : ControllerBase
     {
         private readonly BriqueArcContext _context;
 
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="context">Contexte de l'API</param>
         public RankingsController(BriqueArcContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Demande la liste de tous les scores trié du plus grand au plus petit
+        /// </summary>
+        /// <returns>La liste des scores triée</returns>
         [HttpGet("scoreboard")]
         public async Task<ActionResult<IEnumerable<Ranking>>> GetFirsts()
         {
@@ -27,7 +38,10 @@ namespace BriqueArcASP.Controllers
         }
 
 
-        // GET: api/Rankings
+        /// <summary>
+        /// Demande la liste non-trié des scores
+        /// </summary>
+        /// <returns>La liste des scores</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Ranking>>> GetRankings()
         {
@@ -36,7 +50,11 @@ namespace BriqueArcASP.Controllers
             return await _context.Rankings.ToListAsync();
         }
 
-        // GET: api/Rankings/5
+        /// <summary>
+        /// Demande un score ayant l'identifiant passé en paramêtre
+        /// </summary>
+        /// <param name="id">L'identifiant</param>
+        /// <returns>Le score</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Ranking>> GetRanking(long id)
         {
@@ -50,9 +68,12 @@ namespace BriqueArcASP.Controllers
             return ranking;
         }
 
-        // PUT: api/Rankings/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
+        /// <summary>
+        /// Ajoute un score à l'aide de la méthode PUT
+        /// </summary>
+        /// <param name="id">L'identifiant</param>
+        /// <param name="ranking">Le score</param>
+        /// <returns>Rien</returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> PutRanking(long id, Ranking ranking)
         {
@@ -82,9 +103,11 @@ namespace BriqueArcASP.Controllers
             return NoContent();
         }
 
-        // POST: api/Rankings
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
+        /// <summary>
+        /// Ajoute un score à l'aide de la méthode POST
+        /// </summary>
+        /// <param name="ranking">Le score à ajouter</param>
+        /// <returns>Le score ajouté</returns>
         [HttpPost]
         public async Task<ActionResult<Ranking>> PostRanking(Ranking ranking)
         {
@@ -94,7 +117,11 @@ namespace BriqueArcASP.Controllers
             return CreatedAtAction("GetRanking", new { id = ranking.RankingId }, ranking);
         }
 
-        // DELETE: api/Rankings/5
+        /// <summary>
+        /// Supprime un score ayant l'identifiant passé en paramêtre
+        /// </summary>
+        /// <param name="id">L'identifiant du score à supprimer</param>
+        /// <returns>Le score supprimé</returns>
         [HttpDelete("{id}")]
         public async Task<ActionResult<Ranking>> DeleteRanking(long id)
         {
