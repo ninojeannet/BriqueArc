b03d51cbaab68f5d18efadb1fe1876c51e298086
Yohann.Loison <Yohann.Loison@he-arc.ch>
2020-05-02 10:59:13 +0200

    Fin commentaires ASP

--- BriqueArcASP/BriqueArcASP/Models/Ranking.cs
+++ BriqueArcASP/BriqueArcASP/Models/Ranking.cs
@@ -9,10 +9,19 @@ namespace BriqueArcASP.Models
 {
     public class Ranking
     {
+        /// <summary>
+        /// Identifiant du score
+        /// </summary>
         public long RankingId { get; set; }
 
+        /// <summary>
+        /// Le score
+        /// </summary>
         public int Score { get; set; }
 
+        /// <summary>
+        /// L'identifiant de l'utilisateur ayant fait ce score
+        /// </summary>
         public long UserID { get; set; }
     }
 }
--- BriqueArcASP/BriqueArcASP/Models/User.cs
+++ BriqueArcASP/BriqueArcASP/Models/User.cs
@@ -7,8 +7,19 @@ namespace BriqueArcASP.Models
 {
     public class User
     {
+        /// <summary>
+        /// Identifiant de l'utilisateur
+        /// </summary>
         public long UserId { get; set; }
+
+        /// <summary>
+        /// Nom de compte
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Mot de passe hashé
+        /// </summary>
         public string Password { get; set; }
     }
 }
